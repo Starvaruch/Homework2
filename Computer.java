0598bc8cdf41804cd55d0126554542270056f141
Starvaruch <113064906+Starvaruch@users.noreply.github.com>
2022-12-28 23:56:38 +0300

    Create Computer.java

---
+++ Computer.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class Computer {
+  protected String OZY;
+  protected String disk;
+  protected String monitor;
+  protected String videokarta;
+  protected String keyboard;
+  protected String processor;
+  
+  public String getOZY() {
+      return OZY;
+  }
+  public void setOZY(String OZY) {
+      this.OZY = OZY;
+  }
+  public String getdisk() {
+      return disk;
+  }
+  public void setdisk(String disk) {
+      this.disk = disk;
+  }
+  public String getmonitor() {
+      return monitor;
+  }
+  public void setmonitor(String monitor) {
+      this.monitor = monitor;
+  }
+  public String getvideokarta() {
+      return videokarta;
+  }
+  public void setvideokarta(String videokarta) {
+      this.videokarta = videokarta;
+  }
+  public String getkeyboard() {
+      return keyboard;
+  }
+  public void setkeyboard(String keyboard) {
+      this.keyboard = keyboard;
+  }
+  public String getprocessor() {
+      return processor;
+  }
+  public void setprocessor(String processor) {
+      this.processor = processor;
+  }
+}
