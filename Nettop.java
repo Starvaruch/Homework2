8df1c5cdca42c9aa09880d0a9721ec10fd5a8f29
Starvaruch <113064906+Starvaruch@users.noreply.github.com>
2022-12-29 13:07:21 +0300

    Create Nettop.java

---
+++ Nettop.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Nettop {
+    private Computer comp;
+    
+    public Nettop(){
+        this.comp = new comp();
+    }
+    public String getprocessor() {
+        return comp.getprocessor();
+    }
+    public String getOZY() {
+        return comp.getOZY();
+    }
+    public String getdisk() {
+        return comp.getdisk();
+    }
+}
